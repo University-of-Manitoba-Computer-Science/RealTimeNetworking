c0102ddcf75d00a0e53391bb303f898de55c6ca1
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-02-05 17:03:12 -0600

    rework initialization code

--- jacob-CAN/src/can.c
+++ jacob-CAN/src/can.c
@@ -22,13 +22,29 @@ void canInit()
     MCLK_REGS->MCLK_AHBMASK |= MCLK_AHBMASK_CAN0_Msk;
 
     // Configuration
-    CAN0_REGS->CAN_CCCR |= CAN_CCCR_INIT_Msk;
-    CAN0_REGS->CAN_CCCR |= CAN_CCCR_CCE_Msk | CAN_CCCR_TEST_Msk;
+    CAN0_REGS->CAN_CCCR |= CAN_CCCR_INIT_Msk; // start initialization
+    CAN0_REGS->CAN_CCCR |= CAN_CCCR_CCE_Msk;  // enable configuration
 
-    CAN0_REGS->CAN_TEST = CAN_TEST_LBCK_Msk; // Enable loopback mode
-    // TODO set filter config?
-    // TODO I think Rx Buffer configuration is fine by default?
-    CAN0_REGS->CAN_TXBC = CAN_TXBC_TFQS(32); // TODO I think we want this?
+    // TODO remove once CAN driver is working over loopback
+    CAN0_REGS->CAN_CCCR |= CAN_CCCR_TEST_Msk; // Enable test mode
+    CAN0_REGS->CAN_TEST  = CAN_TEST_LBCK_Msk; // Enable loopback mode
+
+    // Reject non-matching frames
+    // TODO I don't know if we need this, but testing some filtering seems like
+    // a good idea
+    CAN0_REGS->CAN_GFC |= CAN_GFC_ANFS(2) | CAN_GFC_ANFE(2);
+
+    // TODO do we need to set bit rate? Using defaults for now
+
+    // Setting up message RAM
+    CAN0_REGS->CAN_SIDFC = CAN_SIDFC_FLSSA(0) | CAN_SIDFC_LSS(128);
+    CAN0_REGS->CAN_XIDFC = CAN_XIDFC_FLESA(128) | CAN_XIDFC_LSE(64);
+    CAN0_REGS->CAN_RXF0C =
+        CAN_RXF0C_F0OM_Msk | CAN_RXF0C_F0S(0) | CAN_RXF0C_F0SA(256);
+    CAN0_REGS->CAN_RXF0C =
+        CAN_RXF0C_F0OM_Msk | CAN_RXF0C_F0S(0) | CAN_RXF0C_F0SA(1408);
+    CAN0_REGS->CAN_TXBC = CAN_TXBC_TFQM_Msk | CAN_TXBC_TFQS(32) |
+                          CAN_TXBC_NDTB(32) | CAN_TXBC_TBSA(3776);
 
     // unset CCCR.INIT once synchronized
     while ((CAN0_REGS->CAN_CCCR & CAN_CCCR_INIT_Msk) != CAN_CCCR_INIT_Msk)
