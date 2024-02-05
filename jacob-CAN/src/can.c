863e00362a49abda9864d4a6836a3518f62fccc8
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-02-05 15:58:49 -0600

    setup ports

--- jacob-CAN/src/can.c
+++ jacob-CAN/src/can.c
@@ -4,6 +4,16 @@
 
 void canInit()
 {
+    // setup TX port
+    PORT_REGS->GROUP[0].PORT_DIRSET      = PORT_PA22;
+    PORT_REGS->GROUP[0].PORT_PINCFG[22] |= PORT_PINCFG_PMUXEN_Msk;
+    PORT_REGS->GROUP[0].PORT_PMUX[11]   |= PORT_PMUX_PMUXE_I;
+
+    // setup RX port
+    PORT_REGS->GROUP[0].PORT_DIRCLR      = PORT_PA23;
+    PORT_REGS->GROUP[0].PORT_PINCFG[23] |= PORT_PINCFG_PMUXEN_Msk;
+    PORT_REGS->GROUP[0].PORT_PMUX[11]   |= PORT_PMUX_PMUXO_I;
+
     // setup clock
     GCLK_REGS->GCLK_PCHCTRL[27] = GCLK_PCHCTRL_CHEN_Msk;
     while ((GCLK_REGS->GCLK_PCHCTRL[27] & GCLK_PCHCTRL_CHEN_Msk) !=
@@ -15,7 +25,7 @@ void canInit()
     CAN0_REGS->CAN_CCCR |= CAN_CCCR_INIT_Msk;
     CAN0_REGS->CAN_CCCR |= CAN_CCCR_CCE_Msk | CAN_CCCR_TEST_Msk;
 
-    CAN0_REGS->CAN_TEST = CAN_TEST_LBCK_Msk; // Set loopback mode
+    CAN0_REGS->CAN_TEST = CAN_TEST_LBCK_Msk; // Enable loopback mode
     // TODO set filter config?
     // TODO I think Rx Buffer configuration is fine by default?
     CAN0_REGS->CAN_TXBC = CAN_TXBC_TFQS(32); // TODO I think we want this?
