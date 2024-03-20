102810f213b7a6dd79fae163f48a2ae7ecb384c5
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-03-20 09:54:48 -0500

    configure for CAN1

--- jacob-CAN/src/can.c
+++ jacob-CAN/src/can.c
@@ -84,14 +84,14 @@ static int encode_dlc(int len)
 void canInit()
 {
     // setup TX port
-    PORT_REGS->GROUP[0].PORT_DIRSET      = PORT_PA22;
-    PORT_REGS->GROUP[0].PORT_PINCFG[22] |= PORT_PINCFG_PMUXEN_Msk;
-    PORT_REGS->GROUP[0].PORT_PMUX[11]   |= PORT_PMUX_PMUXE_I;
+    PORT_REGS->GROUP[1].PORT_DIRSET      = PORT_PB12;
+    PORT_REGS->GROUP[1].PORT_PINCFG[12] |= PORT_PINCFG_PMUXEN_Msk;
+    PORT_REGS->GROUP[1].PORT_PMUX[6]    |= PORT_PMUX_PMUXE_H;
 
     // setup RX port
-    PORT_REGS->GROUP[0].PORT_DIRCLR      = PORT_PA23;
-    PORT_REGS->GROUP[0].PORT_PINCFG[23] |= PORT_PINCFG_PMUXEN_Msk;
-    PORT_REGS->GROUP[0].PORT_PMUX[11]   |= PORT_PMUX_PMUXO_I;
+    PORT_REGS->GROUP[1].PORT_DIRCLR      = PORT_PB13;
+    PORT_REGS->GROUP[1].PORT_PINCFG[13] |= PORT_PINCFG_PMUXEN_Msk;
+    PORT_REGS->GROUP[1].PORT_PMUX[6]    |= PORT_PMUX_PMUXO_H;
 
     // setup mode port
     PORT_REGS->GROUP[0].PORT_DIRSET = PORT_PA18;
@@ -107,75 +107,75 @@ void canInit()
            GCLK_SYNCBUSY_GENCTRL_GCLK3); /* Wait for synchronization */
 
     // setup clock
-    GCLK_REGS->GCLK_PCHCTRL[CAN0_GCLK_ID] =
+    GCLK_REGS->GCLK_PCHCTRL[CAN1_GCLK_ID] =
         GCLK_PCHCTRL_GEN_GCLK3 | GCLK_PCHCTRL_CHEN_Msk;
-    while ((GCLK_REGS->GCLK_PCHCTRL[CAN0_GCLK_ID] & GCLK_PCHCTRL_CHEN_Msk) !=
+    while ((GCLK_REGS->GCLK_PCHCTRL[CAN1_GCLK_ID] & GCLK_PCHCTRL_CHEN_Msk) !=
            GCLK_PCHCTRL_CHEN_Msk);
-    MCLK_REGS->MCLK_AHBMASK |= MCLK_AHBMASK_CAN0_Msk;
+    MCLK_REGS->MCLK_AHBMASK |= MCLK_AHBMASK_CAN1_Msk;
 
     // Configuration
-    CAN0_REGS->CAN_CCCR |= CAN_CCCR_INIT_Msk; // start initialization
-    CAN0_REGS->CAN_CCCR |= CAN_CCCR_CCE_Msk;  // enable configuration
+    CAN1_REGS->CAN_CCCR |= CAN_CCCR_INIT_Msk; // start initialization
+    CAN1_REGS->CAN_CCCR |= CAN_CCCR_CCE_Msk;  // enable configuration
 
     // TODO remove once CAN driver is working over loopback
     // TODO this prevents the endless stream of TX
-    CAN0_REGS->CAN_CCCR |= CAN_CCCR_TEST_Msk; // Enable test mode
-    CAN0_REGS->CAN_TEST  = CAN_TEST_LBCK_Msk; // Enable loopback mode
+    CAN1_REGS->CAN_CCCR |= CAN_CCCR_TEST_Msk; // Enable test mode
+    CAN1_REGS->CAN_TEST  = CAN_TEST_LBCK_Msk; // Enable loopback mode
 
     // Accept non-matching standard frames in FIFO0
     // Reject non-matching extended frames
-    CAN0_REGS->CAN_GFC |= CAN_GFC_ANFS(0) | CAN_GFC_ANFE(2);
+    CAN1_REGS->CAN_GFC |= CAN_GFC_ANFS(0) | CAN_GFC_ANFE(2);
 
     // Setting up message RAM
     uint32_t *addr = msg_ram;
 
     // Standard filter RAM conf
-    CAN0_REGS->CAN_SIDFC = CAN_SIDFC_FLSSA(addr) | CAN_SIDFC_LSS(STD_FILT_SIZE);
+    CAN1_REGS->CAN_SIDFC = CAN_SIDFC_FLSSA(addr) | CAN_SIDFC_LSS(STD_FILT_SIZE);
     addr += STD_FILT_SIZE * STD_FILT_ELEMENT_SIZE;
 
     // Extended filter RAM conf
-    CAN0_REGS->CAN_XIDFC = CAN_XIDFC_FLESA(addr) | CAN_XIDFC_LSE(EXT_FILT_SIZE);
+    CAN1_REGS->CAN_XIDFC = CAN_XIDFC_FLESA(addr) | CAN_XIDFC_LSE(EXT_FILT_SIZE);
     addr += EXT_FILT_SIZE * EXT_FILT_ELEMENT_SIZE;
 
     // RX FIFO 0 RAM conf
-    CAN0_REGS->CAN_RXF0C = CAN_RXF0C_F0OM_Msk | CAN_RXF0C_F0SA(addr) |
+    CAN1_REGS->CAN_RXF0C = CAN_RXF0C_F0OM_Msk | CAN_RXF0C_F0SA(addr) |
                            CAN_RXF0C_F0S(RX_FIFO0_SIZE);
     addr += RX_FIFO0_SIZE * RX_FIFO0_ELEMENT_SIZE;
 
     // RX FIFO 1 RAM conf
-    CAN0_REGS->CAN_RXF1C = CAN_RXF1C_F1OM_Msk | CAN_RXF1C_F1SA(addr) |
+    CAN1_REGS->CAN_RXF1C = CAN_RXF1C_F1OM_Msk | CAN_RXF1C_F1SA(addr) |
                            CAN_RXF1C_F1S(RX_FIFO1_SIZE);
     addr += RX_FIFO1_SIZE * RX_FIFO1_ELEMENT_SIZE;
 
     // RX Buffer RAM conf
-    CAN0_REGS->CAN_RXBC  = CAN_RXBC_RBSA(addr);
+    CAN1_REGS->CAN_RXBC  = CAN_RXBC_RBSA(addr);
     addr                += RX_BUFFER_SIZE * RX_BUFFER_ELEMENT_SIZE;
     // TODO why isn't there an option to set the buffer size?
 
     // TX Event FIFO RAM conf
-    CAN0_REGS->CAN_TXEFC =
+    CAN1_REGS->CAN_TXEFC =
         CAN_TXEFC_EFSA(addr) | CAN_TXEFC_EFS(TX_EVENT_FIFO_SIZE);
     addr += TX_EVENT_FIFO_SIZE * TX_EVENT_FIFO_ELEMENT_SIZE;
 
     // TX Buffer RAM conf
-    CAN0_REGS->CAN_TXBC =
+    CAN1_REGS->CAN_TXBC =
         CAN_TXBC_TBSA(addr) | CAN_TXBC_NDTB(0) | CAN_TXBC_TFQS(TX_BUFFER_SIZE);
 
-    for (int i = 0; i < TX_BUFFER_SIZE; ++i) CAN0_REGS->CAN_TXBTIE |= (1 << i);
+    for (int i = 0; i < TX_BUFFER_SIZE; ++i) CAN1_REGS->CAN_TXBTIE |= (1 << i);
 
     // unset CCCR.INIT once synchronized
-    while ((CAN0_REGS->CAN_CCCR & CAN_CCCR_INIT_Msk) != CAN_CCCR_INIT_Msk);
-    CAN0_REGS->CAN_CCCR &= ~CAN_CCCR_INIT_Msk;
+    while ((CAN1_REGS->CAN_CCCR & CAN_CCCR_INIT_Msk) != CAN_CCCR_INIT_Msk);
+    CAN1_REGS->CAN_CCCR &= ~CAN_CCCR_INIT_Msk;
 
     PORT_REGS->GROUP[0].PORT_OUTCLR = PORT_PA18;
 }
 
 void queue_message(uint8_t *data, int len)
 {
-    if ((CAN0_REGS->CAN_TXFQS & CAN_TXFQS_TFQF_Msk) == CAN_TXFQS_TFQF_Msk)
+    if ((CAN1_REGS->CAN_TXFQS & CAN_TXFQS_TFQF_Msk) == CAN_TXFQS_TFQF_Msk)
         return; // don't send message if the queue is full
 
-    uint32_t status = CAN0_REGS->CAN_TXFQS & CAN_TXFQS_TFQPI_Msk;
+    uint32_t status = CAN1_REGS->CAN_TXFQS & CAN_TXFQS_TFQPI_Msk;
     uint8_t  index  = status >> CAN_TXFQS_TFQPI_Pos;
 
     int offset        = TX_BUFFER_OFFSET + (index * TX_BUFFER_ELEMENT_SIZE);
@@ -188,12 +188,12 @@ void queue_message(uint8_t *data, int len)
     dbg_write_u8(&index, len);
     dbg_write_char('\n');
 
-    CAN0_REGS->CAN_TXBAR |= 1 << index;
+    CAN1_REGS->CAN_TXBAR |= 1 << index;
 }
 
 int dequeue_message(uint8_t *data, int max_size)
 {
-    int status = CAN0_REGS->CAN_RXF0S;
+    int status = CAN1_REGS->CAN_RXF0S;
 
     int fill_level = (status & CAN_RXF0S_F0FL_Msk) >> CAN_RXF0S_F0FL_Pos;
     int get_index  = (status & CAN_RXF0S_F0GI_Msk) >> CAN_RXF0S_F0GI_Pos;
@@ -203,7 +203,7 @@ int dequeue_message(uint8_t *data, int max_size)
         return -1;
 
     // acknowledge
-    CAN0_REGS->CAN_RXF0A = CAN_RXF0A_F0AI(get_index);
+    CAN1_REGS->CAN_RXF0A = CAN_RXF0A_F0AI(get_index);
 
     int offset = RX_FIFO0_OFFSET + (get_index * RX_FIFO0_ELEMENT_SIZE);
 
