56a41b3940e683eeda8cefd1a71b5d61998795e3
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-04-08 18:49:13 -0500

    added fan code

---
+++ include/fan.h
@@ -0,0 +1,19 @@
+#include "sam.h"
+#include <stdint.h>
+
+#define NUM_SAMPLES 16
+
+void updateOutput(uint8_t dutyCycle);
+
+void fanInit();
+
+void rpmInit();
+
+// clock is setup to ensure that it never overflows within our RPM range; an overflow indicates no pulse, or that the fan has stopped
+void TCC0_OTHER_Handler();
+
+
+// we do need averaging as there is some jitter in the feedback from the fan
+
+// ISR for match capture events
+void TCC0_MC0_Handler();
